afbe4cfb426a8a28476e00ecb56b4ae56ee8c230
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-16 09:15:57 -0500

    revamped fetching funcs using async/await

--- app/src/fetch-functions.js
+++ app/src/fetch-functions.js
@@ -1,43 +1,39 @@
 const comicsUrl = "https://xkcd.vercel.app/?comic=latest";
 
-export const getLatestComic = () => {
-  return fetch(comicsUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(
-          `Failed to get latest comic! ${response.status} ${response.statusText}`
-        );
-      }
-      return response.json();
-    })
-    .then((comicBody) => {
-      console.log(comicBody);
-      return comicBody;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return null;
-    });
+export const getLatestComic = async () => {
+  try {
+    const response = await fetch(comicsUrl);
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to get latest comic! ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.warn(error);
+    return null;
+  }
 };
 
-export const getSpecificComic = (comicNum) => {
+export const getSpecificComic = async (comicNum) => {
   const comicUrl = `https://xkcd.vercel.app/?comic=${comicNum}`;
 
-  return fetch(comicUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(
-          `Failed to get this specific comic! ${response.status} ${response.statusText}`
-        );
-      }
-      return response.json();
-    })
-    .then((comicBody) => {
-      console.log(comicBody);
-      return comicBody;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return null;
-    });
+  try {
+    const response = await fetch(comicUrl);
+
+    if (!response.ok) {
+      throw Error(
+        `Failed to get this specific comic! ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.warn(error);
+    return null;
+  }
 };
