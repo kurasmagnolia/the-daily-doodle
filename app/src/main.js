afa035aa45f411cd1f0dd9bb74045778ce4c18e6
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-18 00:29:59 -0500

    continuing html starter skeleton

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,19 @@
 import { getLatestComic, getSpecificComic } from "./fetch-functions";
 
-const main = () => {
+const main = async () => {
   getLatestComic();
   getSpecificComic(235);
+
+  const latestComic = await getLatestComic();
+
+  // console.log(latestComic);
+  const randomGen = Math.floor(Math.random() * latestComic.num);
+
+  console.log(await getSpecificComic(randomGen));
+
+  const comics = await fetch9RandomComics();
+
+  // render3x3(comicDiv, comics);
 };
 
 main();
