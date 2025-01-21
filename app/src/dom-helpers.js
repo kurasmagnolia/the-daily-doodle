9c27e6d693ccf8296d95d83c48d8a4f1e8559191
Tyron <tyronodame5@gmail.com>
2025-01-21 18:06:55 -0500

    final changes to the homepage

--- app/src/dom-helpers.js
+++ app/src/dom-helpers.js
@@ -2,12 +2,12 @@ import {
   getLatestComic,
   getSpecificComic,
   getFeaturedComic,
-} from "./fetch-functions";
+} from './fetch-functions';
 
 export const renderFeatureComic = (comic) => {
-  const comicDiv = document.getElementById("");
-  const comicHeading = document.getElementById("");
-  const comicImage = document.createElement("img");
+  const comicDiv = document.getElementById('');
+  const comicHeading = document.getElementById('');
+  const comicImage = document.createElement('img');
 
   comicHeading.textContent = `Featured Comic: ${comic.title} `;
 
@@ -19,17 +19,17 @@ export const renderFeatureComic = (comic) => {
 
 export const renderRandom = (comicDiv, comics) => {
   // clears each row before inserting its 3 comic strip images
-  comicDiv.innerHTML = "";
+  comicDiv.innerHTML = '';
 
   // iterate through comics array and create cell for it
   comics.forEach((comic) => {
-    const comicCell = document.createElement("div");
-    const comicImage = document.createElement("img");
+    const comicCell = document.createElement('div');
+    const comicImage = document.createElement('img');
 
     // give elements their attributes and content
-    comicCell.className = "comic-cell";
+    comicCell.className = 'comic-cell';
     comicImage.src = comic.src;
-    comicImage.className = "comic-panel";
+    comicImage.className = 'comic-panel';
     comicImage.alt = comic.alt;
 
     // append elements to its correct container
@@ -39,32 +39,32 @@ export const renderRandom = (comicDiv, comics) => {
   });
 };
 
+// grid-template-columns: 1fr 1fr 1fr;
+
 export const render3x3 = (comicDiv, comics) => {
   // clears each row before inserting its 3 comic strip images
-  comicDiv.innerHTML = "";
+  comicDiv.innerHTML = '';
 
   // iterate through comics array and create cell for it
   comics.forEach((comic) => {
-    const comicCell = document.createElement("div");
-    const comicImage = document.createElement("img");
+    const comicImage = document.createElement('img');
 
     // give elements their attributes and content
-    comicCell.className = "comic-cell";
     comicImage.src = comic.img;
-    comicImage.className = "comic-panel";
+    comicImage.className = 'comic-panel';
     comicImage.alt = comic.alt;
 
     // append elements to its correct container
-    comicCell.append(comicImage);
-    comicDiv.append(comicCell);
+    // comicCell.append(comicImage);
+    // comicDiv.append(comicCell);
     comicDiv.append(comicHeading, comicImage);
   });
 };
 
 export const renderFavorites = (favoritesUl, comic) => {
-  const comicDiv = document.createElement("div");
-  const comicImg = document.createElement("img");
-  const comicHeading = document.createElement("h3");
+  const comicDiv = document.createElement('div');
+  const comicImg = document.createElement('img');
+  const comicHeading = document.createElement('h3');
 
   comicImg.src = comic.img;
   comicImg.alt = comic.alt;
