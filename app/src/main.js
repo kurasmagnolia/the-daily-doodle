b6df8b2366cfc9d7c6fd9363ded297c26d14d9f9
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-21 18:31:58 -0500

    created + added favicon, created counter.js, adding cursor styling, and commenting code out in main.js (restructuring)

--- app/src/main.js
+++ app/src/main.js
@@ -2,17 +2,18 @@ import { getLatestComic, getSpecificComic } from "./fetch-functions";
 import { activeNavPage } from "./dom-helpers";
 
 const main = async () => {
-  getLatestComic();
-  getSpecificComic(235);
+  const comicDiv = document.getElementById("comic-grid");
+  // getLatestComic();
+  // getSpecificComic(235);
 
-  const latestComic = await getLatestComic();
+  // const latestComic = await getLatestComic();
 
-  // console.log(latestComic);
-  const randomGen = Math.floor(Math.random() * latestComic.num);
+  // // console.log(latestComic);
+  // const randomGen = Math.floor(Math.random() * latestComic.num);
 
-  console.log(await getSpecificComic(randomGen));
+  // console.log(await getSpecificComic(randomGen));
 
-  const comics = await fetch9RandomComics();
+  // const comics = await fetch9RandomComics();
 
   // render3x3(comicDiv, comics);
 };
