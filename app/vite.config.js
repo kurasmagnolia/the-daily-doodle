6c29773a067789100ddbb43cb41a053050cdc947
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-23 11:37:50 -0500

    fixed vite.config.js 2x

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  // Keep the forward slashes / / around your repo name
-  base: "/the-daily-doodle",
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: {
+    outDir: "..",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        page1: "./src/comic-generator.html",
+        page2: "./src/favorites.html",
+      },
+    },
+  },
+  // needed for github pages just put the repo name here
+  base: "/the-daily-doodle/",
 });
