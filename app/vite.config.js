51adb408b952edf19b0657bbdb4cc3e1209fc9fe
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-22 23:55:52 -0500

    pushing deployment changes

---
+++ app/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Keep the forward slashes / / around your repo name
+  base: "https://github.com/kurasmagnolia/the-daily-doodle",
+});
