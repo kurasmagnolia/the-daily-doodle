b0e4c54c971a9fe15b16e2caabd0c4d6bc6a7fe8
kurasmagnolia <kingmmcleod@outlook.com>
2025-01-27 12:52:21 -0500

    pushing second to last feature changes

--- app/src/comic.js
+++ app/src/comic.js
@@ -1,29 +1,49 @@
 import {
   handlePrevClick,
-  handleInputChange,
   handleNextClick,
   handleRandomClick,
-} from './handle-funcs';
+  handleInputSubmit,
+  handleComicClick,
+  handleFavoriteClick,
+  updateHeartIcons,
+} from "./handle-funcs";
 
 const main = () => {
-  const prevButton = document.querySelector('#prev-btn');
-  console.log('test');
-  console.log(prevButton);
-  prevButton.addEventListener('click', handlePrevClick);
+  const closeButton = document.querySelector(".close-modal"); // selects the close button
+  const prevButton = document.querySelector("#prev-btn");
+
+  prevButton.addEventListener("click", handlePrevClick);
 
   document
-    .getElementById('random-btn')
-    .addEventListener('click', handleRandomClick);
-  console.log('random test');
+    .getElementById("random-btn")
+    .addEventListener("click", handleRandomClick);
+  console.log("random test");
 
   document
-    .getElementById('next-btn')
-    .addEventListener('click', handleNextClick);
+    .getElementById("next-btn")
+    .addEventListener("click", handleNextClick);
 
-  // event listener for the input field
   document
-    .getElementById('submit-btn')
-    .addEventListener('click', handleInputChange);
+    .querySelector(".user-input-form")
+    .addEventListener("submit", handleInputSubmit);
+
+  document.addEventListener("click", (event) => {
+    const heartButton = event.target.closest(".favorite-button");
+    if (heartButton) {
+      handleFavoriteClick(event);
+    }
+  });
+
+  document.addEventListener("click", handleComicClick);
+
+  //  event listener for the close button
+  closeButton.addEventListener("click", () => {
+    const dialog = document.querySelector("dialog");
+    dialog.close(); // closes the modal
+
+    // After closing the modal, update the heart icons in the grid
+    updateHeartIcons(); // Ensure the heart icons in the grid are updated based on the localStorage state
+  });
 };
 
 main();
